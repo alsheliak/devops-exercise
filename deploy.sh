d444a1958a3a4fbd7cf3aeab23c8a8672d1fb2a8
Elad Elgrabli <elad@ownbackup.com>
2019-02-25 04:28:05 +0200

    dockersucks

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,5 @@
 docker ps --filter "name=node_app"
 docker pull $IMAGE
 docker rm -f node_app || true
-docker run -d \
-           --name node_app \
-           --restart unless-stopped \
-           -p 80:3000 \
-           $IMAGE
+docker run -d --name node_app --restart unless-stopped -p 80:3000 $IMAGE
 docker ps --filter "name=node_app"
